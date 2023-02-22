7b0c87d9affc8e0f40708c334530a9e2706f0f7b
shahfarazz <shahfarazulhaq@gmail.com>
2023-02-22 16:21:05 +0500

    connected to mongodb

---
+++ pages/api/seed.js
@@ -0,0 +1,13 @@
+const { default: User } = require('@/models/User');
+const { default: data } = require('@/utils/data');
+const { default: db } = require('@/utils/db');
+
+const handler = async (req, res) => {
+	await db.connect();
+	await User.deleteMany();
+	await User.insertMany(data.users);
+	await db.disconnect();
+	res.send({ message: 'seeded successfully' });
+};
+
+export default handler;
--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+	users: [
+		{
+			name: 'Faraz',
+			email: 'admin@example.com',
+			password: bcrypt.hashSync('123456'),
+			isAdmin: true,
+		},
+		{
+			name: 'Fawaz',
+			email: 'user@example.com',
+			password: bcrypt.hashSync('123456'),
+			isAdmin: false,
+		},
+	],
 	products: [
 		{
 			name: 'Dress 1',
